e18e8961896f8d428b6c48bbaa05f7f167fe872b
A U Thor <belaardon@gmail.com>
2022-03-14 14:55:20 +0100

    initial project, create modules

---
+++ EcommerceProject/EcommerceWebParent/EcommerceBackEnd/src/main/java/com/ecommerce/admin/MainController.java
@@ -0,0 +1,16 @@
+package com.ecommerce.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	
+	@GetMapping("")
+	public String viewHomePage() {
+		return "index";
+	}
+	
+	
+}
