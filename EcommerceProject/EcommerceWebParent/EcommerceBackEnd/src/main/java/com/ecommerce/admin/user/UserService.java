b438a94aa3e96057bef8aafb2aac65d224c02043
ar-don <bela.ardon@gmail.com>
2022-03-14 21:24:07 +0100

    Code new user function and encode password for users

--- EcommerceProject/EcommerceWebParent/EcommerceBackEnd/src/main/java/com/ecommerce/admin/user/UserService.java
+++ EcommerceProject/EcommerceWebParent/EcommerceBackEnd/src/main/java/com/ecommerce/admin/user/UserService.java
@@ -3,18 +3,41 @@ package com.ecommerce.admin.user;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.ecommerce.common.entity.Role;
 import com.ecommerce.common.entity.User;
 
 @Service
 public class UserService {
 	
 	@Autowired
-	private UserRepository repo;
+	private UserRepository userRepo;
+
+	@Autowired
+	private RoleRepository roleRepo;
 	
-	public List<User> listAll(){
-		return (List<User>) repo.findAll();
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+
+	public List<User> listAll() {
+		return (List<User>) userRepo.findAll();
+	}
+
+	public List<Role> listRoles() {
+		return (List<Role>) roleRepo.findAll();
+	}
+
+	public void save(User user) {
+		encodePassword(user);
+		userRepo.save(user);
+	}
+	
+	private void encodePassword(User user) {
+		String encodedPassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encodedPassword);
 	}
 
 }
