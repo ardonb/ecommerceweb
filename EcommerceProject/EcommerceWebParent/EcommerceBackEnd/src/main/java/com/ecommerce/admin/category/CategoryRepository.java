e677582b1dc4fce4ddf2851d284f841310681aa4
Ardon Bela <ardon.bela@gmail.com>
2022-03-21 22:37:32 +0100

    Code sorting and enable status for Category

--- EcommerceProject/EcommerceWebParent/EcommerceBackEnd/src/main/java/com/ecommerce/admin/category/CategoryRepository.java
+++ EcommerceProject/EcommerceWebParent/EcommerceBackEnd/src/main/java/com/ecommerce/admin/category/CategoryRepository.java
@@ -2,6 +2,8 @@ package com.ecommerce.admin.category;
 
 import java.util.List;
 
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
@@ -10,6 +12,14 @@ import com.ecommerce.common.entity.Category;
 public interface CategoryRepository extends PagingAndSortingRepository<Category, Integer> {
 
 	@Query("SELECT c FROM Category c WHERE c.parent.id is NULL")
-	public List<Category> findRootCategories();
+	public List<Category> findRootCategories(Sort sort);
+	
+	public Category findByName(String name);
+
+	public Category findByAlias(String alias);
+	
+	@Query("UPDATE Category c SET c.enabled = ?2 WHERE c.id = ?1")
+	@Modifying
+	public void updateEnabledStatus(Integer id, boolean enabled);	
 	
 }
